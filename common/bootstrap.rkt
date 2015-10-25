591715191de419f3e451622ddef3be7fc30d131b
gregr <greg.weiqi@gmail.com>
2015-10-25 16:37:14 -0400

    Add bootstrap.rkt

---
+++ common/bootstrap.rkt
@@ -0,0 +1,33 @@
+#lang racket/base
+(provide
+  network
+  storage
+  )
+
+(require
+  "basis.rkt"
+  gregr-misc/oop
+  )
+
+(define network
+  (let ((connection (class _ (conn) () ()
+                      (close () (network-close conn))
+                      (send (value) (network-send conn value))
+                      (recv () (network-recv conn)))))
+    (object-new '()
+      (method-table _
+        (listen () (connection (network-listen)))
+        (connect (hostname) (connection (network-connect hostname)))))))
+
+(define storage
+  (let ((mbr (object-new '()
+               (method-table _
+                 (get () (storage-mbr-get))
+                 (put (value) (storage-mbr-put value))
+                 (delete () (storage-mbr-delete))))))
+    (object-new '()
+      (method-table _
+        (get (path) (storage-get path))
+        (put (path value) (storage-put path value))
+        (delete (path) (storage-delete path))
+        (mbr () mbr)))))
